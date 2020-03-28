6844835cc9e0f6e7ced0144486a3d230332321ac
Jeovania <jeovaniaabreu@gmail.com>
2020-03-28 14:16:27 -0300

    Add app structure

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  trailingComma: 'all',
+}
